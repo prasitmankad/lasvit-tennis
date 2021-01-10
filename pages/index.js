55c0a8414b20bb84909944f61f1a2f22285aaec9
hyperic <prasit.mankad@gmail.com>
2021-01-10 19:42:06 +1100

    Title Linkage

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ function IndexPage(props) {
           </div>
           <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 font-bold">
-              {configData.title}
+              {props.configData.content[0].heading}
             </h1>
             <h2 class="title-font sm:text-1xl text-2xl mb-4 font-medium text-gray-900">
               {configData.tagline}
