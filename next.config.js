9c204d79bd89d1f7763c0ef3a6f778a2a3169b79
hyperic <prasit.mankad@gmail.com>
2021-01-18 23:17:27 +1100

    Daily Checkin

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const STUDIO_REWRITE = {
-  source: '/studio/:path*',
+  source: "/studio/:path*",
   destination:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3333/studio/:path*'
-      : '/studio/index.html',
-}
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
 
 module.exports = {
   rewrites: () => [STUDIO_REWRITE],
-}
+  trailingSlash: true,
+};
