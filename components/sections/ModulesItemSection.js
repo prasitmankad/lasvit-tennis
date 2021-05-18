4a5553b9dd986c761543fb1ed46961cac82c34b9
Vratislav Beleni <vratislav.beleni@tipsport.cz>
2021-05-18 17:05:39 +0200

    peek

--- components/sections/ModulesItemSection.js
+++ components/sections/ModulesItemSection.js
@@ -21,6 +21,12 @@ export function ModulesItemSection({ items, title }) {
                   height="auto"
                 />
               )}
+
+              {file.contentType !== "video" && (
+                <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+                  // TODO
+                </div>
+              )}
             </div>
             <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
               {file.contentTitle}
