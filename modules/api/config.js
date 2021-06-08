b4a3b608b5f604ca9f9f4b9c8f14c7f1cf1cad53
Jiri Vanek <jirivanek@Jiris-MacBook-Pro.local>
2021-06-08 13:23:46 +0200

    publishable key

--- modules/api/config.js
+++ modules/api/config.js
@@ -2,7 +2,7 @@ export const config = {
   stripe: {
     currency: "CZK",
     apiKey:
-      "pk_test_51IdIGYExvadlvJZ2NGckQcsVFhkmY5kELt2SSTCuSa9icMjmUSrjQuM6FypvlVPMXLmErF2x2IRdbN0eOZZf5BFN00HEDWzxQw",
+      "my_PUBLISHABLE_stripekey",
     apiUrl:
       "https://v6djkdnzx0.execute-api.eu-central-1.amazonaws.com/prod/serverless/createCharge",
   },
